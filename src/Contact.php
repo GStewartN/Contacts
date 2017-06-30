3092655d21038d7ef195de20303bff642f5f727f
GStewartN <stewart.nathang@gmail.com>
2017-06-30 09:23:40 -0700

    declare class get/set/save/all functions

--- src/Contact.php
+++ src/Contact.php
@@ -1,17 +1,72 @@
 <?php
     class Contact
     {
-      private $first_name;
-      private $last_name;
-      private $phone_number;
-      private $address;
-    }
+        private $first_name;
+        private $last_name;
+        private $phone_number;
+        private $address;
 
-    function __construct($first_name, $last_name, $phone_number, $address)
-    {
-        $this->first_name = $first_name;
-        $this->last_name = $last_name;
-        $this->phone_number = $phone_number;
-        $this->address = $address;
+        function __construct($first_name, $last_name, $phone_number, $address)
+        {
+            $this->first_name = $first_name;
+            $this->last_name = $last_name;
+            $this->phone_number = $phone_number;
+            $this->address = $address;
+        }
+
+        function setFirstName($new_first_name)
+        {
+            $this->first_name = (string) $new_first_name;
+        }
+
+        function getFirstName()
+        {
+            return $this->first_name;
+        }
+
+        function setLastName($new_last_name)
+        {
+            $this->last_name = (string) $new_last_name;
+        }
+
+        function getLastName()
+        {
+            return $this->last_name;
+        }
+
+        function setPhoneNumber($new_phone_number)
+        {
+            $this->phone_number = (string) $new_phone_number;
+        }
+
+        function getPhoneNumber()
+        {
+            return $this->phone_number;
+        }
+
+        function setAddress($new_address)
+        {
+            $this->address = (string) $new_address;
+        }
+
+        function getAddress()
+        {
+            return $this->address;
+        }
+
+        function save()
+        {
+            array_push($_SESSION['list_of_contacts'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_contacts'];
+        }
+
+        static function deleteAll()
+        {
+            $_SESSION['list_of_contacts'] = array();
+        }
     }
- ?>
+?>
