d58fbecc59486d93bb692f1b5f1795390fac8b30
GStewartN <stewart.nathang@gmail.com>
2017-06-30 11:09:44 -0700

    fix syntax error, change how list and form display

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     date_default_timezone_set("America/Los_Angeles");
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Places.php";
+    require_once __DIR__."/../src/Contact.php";
 
     session_start();
 
@@ -23,7 +23,7 @@
         $contact = new Contact($_POST['first_name'], $_POST['last_name'], $_POST['phone_number'], $_POST['address']);
         $contact->save();
         return $app['twig']->render('create_contacts.html.twig', array('new_contact' => $contact));
-    })
+    });
 
     return $app;
 ?>
