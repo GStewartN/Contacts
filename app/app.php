1e5d739c87fdd48f591b2184649df98607fb15e8
GStewartN <stewart.nathang@gmail.com>
2017-06-30 10:33:08 -0700

    add contact form and list html to twig/contacts w route in app.php, add created contact html in twig/create w route in app.php

--- app/app.php
+++ app/app.php
@@ -14,4 +14,16 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         "twig.path" => __DIR__."/../views"
     ));
+
+    $app->get("/", function() use ($app) {
+          return $app["twig"]->render("contacts.html.twig", array("contacts" => Contact::getAll()));
+    });
+
+    $app->post("/contacts", function() use ($app) {
+        $contact = new Contact($_POST['first_name'], $_POST['last_name'], $_POST['phone_number'], $_POST['address']);
+        $contact->save();
+        return $app['twig']->render('create_contacts.html.twig', array('new_contact' => $contact));
+    })
+
+    return $app;
 ?>
